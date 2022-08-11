03ab13bb0cbb2791dbbd4a1bd3b57b25f8b3a37a
Ugesh ROOPCHAND <uroopchand@Prodigious.mu>
2022-08-11 15:53:51 +0400

    aria-errormessage

--- src/component/molecules/contactform/Contact.js
+++ src/component/molecules/contactform/Contact.js
@@ -41,7 +41,7 @@ function Contact() {
                 <input id="fname" className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
                  focus:ring-blue-500 focus:border-violet-custom block w-full p-2.5 dark:bg-gray-700
                   dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500
-                   dark:focus:border-blue-500' type="text" placeholder='First Name' aria-describedby='errorMsg' aria-invalid="true" {...register('fname')}/>
+                   dark:focus:border-blue-500' type="text" placeholder='First Name' aria-errormessage='errorMsg' aria-invalid="true" {...register('fname')}/>
             </div>
           
              
